8d1ad4fe41e1addf1fe6b84547b80ada44740e84
Vlad Alexandru Barbu <Vlad.Barbu@autonet.ro>
2024-12-12 19:30:42 +0200

    init

---
+++ client/client.ml
@@ -0,0 +1,31 @@
+open Unix
+
+let connect_local_tcp port timeout_s =
+  let sock = socket PF_INET SOCK_STREAM 0 in
+  let addr = ADDR_INET (inet_addr_loopback, port) in
+  try
+    Unix.setsockopt_float sock SO_RCVTIMEO timeout_s;
+    connect sock addr;
+    Helpers.log_info @@ Printf.sprintf "connected to localhost:%d" port;
+    Ok sock
+  with
+  | exn ->
+    Helpers.log_exn ~ctx:"connect" exn;
+    Error exn
+;;
+
+let () =
+  let port = 3000
+  and timeout_s = 5.0
+  and n = 1_000_000 in
+  match connect_local_tcp port timeout_s with
+  | Ok sock ->
+    (match Helpers.send_tcp sock @@ Bytes.create n with
+     | Ok _ ->
+       Helpers.log_info @@ Printf.sprintf "sent %d bytes to localhost:%d" n port;
+       close sock
+     | Error exn ->
+       Helpers.log_exn ~ctx:"send_tcp" exn;
+       close sock)
+  | Error exn -> Helpers.log_exn ~ctx:"connect" exn
+;;
